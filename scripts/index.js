078677e02cafe4e22021943bd85245e23b8dda96
Arseniy Medyakov <arseniy.medyakov00@mail.ru>
2023-11-23 00:28:24 +0300

    completed-task

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function addCard(cardData, deleteCardCallback) {
+    const cardPlacesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardPlacesItem.querySelector('.card__image');
+    const cardTitle = cardPlacesItem.querySelector('.card__title');
+    
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    
+    const cardDeleteButton = cardPlacesItem.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener("click", () => {
+        deleteCardCallback(cardPlacesItem)
+    });
+    return cardPlacesItem;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(cardPlacesItem) {
+    cardPlacesItem.remove();
+}
 
-// @todo: Функция удаления карточки
+const placesList = document.querySelector(".places__list");
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    const newCard = addCard(item, deleteCard);
+    placesList.append(newCard);
+})

0d8c5c078ea1e12325d31581bf5e6f5c30e6a423
Arseniy Medyakov <arseniy.medyakov00@mail.ru>
2023-12-13 11:44:44 +0300

    remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './pages/index.css';
 import {createCard, likeCard, deleteCard} from './components/card.js'
 import {initialCards} from './components/cards.js'
-import {openPopup, closePopup, cardPopup, changeProfFormSubmit, addCard} from './components/modal.js';
+import {openPopup, cardPopup, changeProfFormSubmit, addCard} from './components/modal.js';
 
-const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 initialCards.forEach((item) => {
@@ -13,21 +12,17 @@ initialCards.forEach((item) => {
 
 const chageProfButton = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
-const closeEdit = document.querySelector('.popup_type_edit .popup__close');
 
 chageProfButton.addEventListener('click', () => {
     openPopup(popupEdit);
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileDescription.textContent;
 })
-closeEdit.addEventListener('click', () => closePopup(popupEdit))
 
 const addCardButton = document.querySelector('.profile__add-button');
 const newCardPopup = document.querySelector('.popup_type_new-card');
-const closeAdd = document.querySelector('.popup_type_new-card .popup__close');
 
 addCardButton.addEventListener('click', () => openPopup(newCardPopup))
-closeAdd.addEventListener('click', () => closePopup(newCardPopup))
 
 const editProfileForm = document.querySelector("[name='edit-profile']")
 const nameInput = document.querySelector('.popup__input_type_name')
@@ -38,11 +33,6 @@ const profileDescription = document.querySelector('.profile__description')
 
 editProfileForm.addEventListener('submit', changeProfFormSubmit);
 
-
-// Добавление новой карточки
-// объявление формы и инпутов
 const newPlaceForm = document.querySelector("[name='new-place']")
-const cardNameInput = document.querySelector('.popup__input_type_card-name')
-const cardUrlInput = document.querySelector('.popup__input_type_url')
 
 newPlaceForm.addEventListener('submit', addCard)
